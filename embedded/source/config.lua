6c373dac445c8fa6b9bb195436f045846462c92b
Stefan Wanitzek <stefan@dreambyte.eu>
2015-08-19 20:56:53 +0200

    Migrate firmware-code from main-repository into firmware-repository

--- embedded/source/config.lua
+++
@@ -1,6 +0,0 @@
--- configuration module
-module("config", package.seeall)
-
-SERVER_HOSTNAME = "example.com" -- hostname of the server which is running our node server
-SERVER_PORT = 80 -- server port (port 80 should work well in most cases)
-SERVER_URL = "/bigredbutton" -- The path to the node-server. Don't include a trailing backslash.
--- embedded/source/init.lua
+++
@@ -1,6 +0,0 @@
--- This file gets executed as soon as the controller is connected to the power supply
-
-print "Booting..."
-
--- We just make a simple call to firmware.lua which contains the main program code
-dofile("firmware.lua")
